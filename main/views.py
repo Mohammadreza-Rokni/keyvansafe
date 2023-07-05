ea17b00c4edcfedeeebac4d1d39e5e31331521e2
Mohammadreza-Rokni <mrokniahmadi.mr@gmail.com>
2023-07-05 20:04:04 +0330

    coming soon is completed

--- main/views.py
+++ main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from .forms import SubscribeForm
+from .models import Subscriber
 def coming_soon(request):
-    return render(request, 'main/coming_soon.html')
+    form = SubscribeForm()
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            subscriber = Subscriber.objects.create(email=email)
+            subscriber.save()
+            return render(request, 'main/thank_you.html')
+    return render(request, 'main/coming_soon.html', {'form': form})
