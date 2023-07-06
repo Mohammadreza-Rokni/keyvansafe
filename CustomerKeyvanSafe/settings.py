b36629f06753b88b66c65a06331ba42a47a2a280
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-07-06 22:33:40 +0330

    without logo

--- CustomerKeyvanSafe/settings.py
+++ CustomerKeyvanSafe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qopx(b#9#t6+w(+pn^nrsi2@_htkw^_(#%*7i7dd%a3%qggm66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
